426f042d2412bfa84511018493af9c949d52f730
huangziyi <huangziyi@canaan-creative.com>
2024-04-30 17:37:38 +0800

    update
    Signed-off-by: huangziyi <huangziyi@canaan-creative.com>

--- clousure_bind.c
+++ clousure_bind.c
@@ -26,8 +26,25 @@ static void gen_machine_code(uint8_t* code, void(* function)(void*), void* conte
     // pop %rbq
     // retq
     memcpy(code, "\xff\xd0\x90\x5d\xc3", 5);
-#elif __riscv__
-
+#elif __riscv
+#if __riscv_xlen == 64
+    // RV64
+    // addi    sp,sp,-32
+    // sd      ra,24(sp)
+    // sd      s0,16(sp)
+    // addi    s0,sp,32
+    memcpy(code, "\x11\x01\xec\x06\xe8\x22\x10\x00", 8);
+    code += 8;
+    // immediate constant context
+    uint32_t highbit = (uint64_t)context & 0xfffff000UL;
+    uint32_t lowbit = (uint64_t)context & 0xfffUL;
+    // addi    a5,x0,%[lowbit]
+    // lui     a5,%[highbit]
+    // TODO
+#else
+    // RV32
+#error Arch not support
+#endif
 #else
 #error Arch not support
 #endif
