bb6c8fd7521a15bc89c6c5145eece9a1961b115c
hzy <i@anclx.cc>
2024-05-02 19:32:16 +0800

    add rv64 support
    Signed-off-by: hzy <i@anclx.cc>

--- clousure_bind.c
+++ clousure_bind.c
@@ -26,21 +26,57 @@ static void gen_machine_code(uint8_t* code, void(* function)(void*), void* conte
     // pop %rbq
     // retq
     memcpy(code, "\xff\xd0\x90\x5d\xc3", 5);
+    code += 5;
 #elif __riscv
 #if __riscv_xlen == 64
     // RV64
-    // addi    sp,sp,-32
-    // sd      ra,24(sp)
-    // sd      s0,16(sp)
-    // addi    s0,sp,32
-    memcpy(code, "\x11\x01\xec\x06\xe8\x22\x10\x00", 8);
+    memcpy(
+        code,
+        // addi    sp,sp,-32
+        "\x01\x11"
+        // sd      ra,24(sp)
+        "\x06\xec"
+        // sd      s0,16(sp)
+        "\x22\xe8"
+        // addi    s0,sp,32
+        "\x00\x10",
+        8
+    );
     code += 8;
-    // immediate constant context
-    uint32_t highbit = (uint64_t)context & 0xfffff000UL;
-    uint32_t lowbit = (uint64_t)context & 0xfffUL;
-    // addi    a5,x0,%[lowbit]
-    // lui     a5,%[highbit]
-    // TODO
+    // immediate constant context to a5, store context to the rest of buffer
+    *(uint64_t*)(code + 1024) = (uint64_t)function;
+    *(uint64_t*)(code + 1032) = (uint64_t)context;
+    memcpy(
+        code,
+        // auipc   a6,0
+        "\x17\x08\x00\x00"
+        // ld      a5,1024(a6)
+        "\x83\x37\x08\x40"
+        // ld      a0,1032(a6)
+        "\x03\x35\x88\x40"
+        // jalr    a5
+        "\x82\x97"
+        ,
+        14
+    );
+    code += 14;
+    // return
+    memcpy(
+        code,
+        // nop
+        "\x01\x00"
+        // ld      ra,24(sp)
+        "\xe2\x60"
+        // ld      s0,16(sp)
+        "\x42\x64"
+        // add     sp,sp,32
+        "\x05\x61"
+        // ret
+        "\x82\x80"
+        ,
+        10
+    );
+    code += 10;
 #else
     // RV32
 #error Arch not support
